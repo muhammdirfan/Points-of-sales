b6537392cb24629f7ce3228ace84fa3317fde581
ShakeelHamiya <shakeelhamiya996@gmail.com>
2021-04-06 06:12:14 -0700

    sales section

--- src/Layout/Dashboard/Childs/Sales/index.js
+++ src/Layout/Dashboard/Childs/Sales/index.js
@@ -1,11 +1,225 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Table,
+  Input,
+  InputNumber,
+  Popconfirm,
+  Form,
+  Typography,
+  Button,
+  Row,
+  Col
+} from "antd";
+import "./Oders.css";
+import Styles from "./style";
+import { styles } from "ansi-colors";
+const originData = [];
+const { Search } = Input;
+const onSearch = value => console.log(value);
 
-const Sales = () => {
+// for (let i = 0; i < 100; i++) {
+//   originData.push({
+//     key: i.toString(),
+//     name: `Edrward ${i}`,
+//     age: 32,
+//     address: `London Park no. ${i}`,
+//   });
+// }
+
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === "number" ? <InputNumber /> : <Input />;
   return (
-    <>
-      <p>THis is Sales side</p>
-    </>
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`
+            }
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
   );
 };
 
-export default Sales;
+const EditableTable = () => {
+  const [form] = Form.useForm();
+  const [data, setData] = useState(originData);
+  const [editingKey, setEditingKey] = useState("");
+
+  const isEditing = record => record.key === editingKey;
+
+  const edit = record => {
+    form.setFieldsValue({
+      name: "",
+      age: "",
+      address: "",
+      ...record
+    });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey("");
+  };
+
+  const save = async key => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex(item => key === item.key);
+
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        setData(newData);
+        setEditingKey("");
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey("");
+      }
+    } catch (errInfo) {
+      console.log("Validate Failed:", errInfo);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Order Id",
+      dataIndex: "order_id",
+      width: "30%",
+      editable: true
+    },
+    {
+      title: "Table No",
+      dataIndex: "table_no",
+      editable: true
+    },
+    {
+      title: "Menu",
+      dataIndex: "menu",
+      width: "30%",
+      editable: true
+    },
+    {
+      title: "Total Amount",
+      dataIndex: "total_amount",
+      editable: true
+    },
+    {
+      title: "operation",
+      dataIndex: "operation",
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <a
+              href="javascript:;"
+              onClick={() => save(record.key)}
+              style={{
+                marginRight: 8
+              }}
+            >
+              Save
+            </a>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Typography.Link
+            disabled={editingKey !== ""}
+            onClick={() => edit(record)}
+          >
+            Edit
+          </Typography.Link>
+        );
+      }
+    }
+  ];
+  const mergedColumns = columns.map(col => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: record => ({
+        record,
+        inputType: col.dataIndex === "age" ? "number" : "text",
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record)
+      })
+    };
+  });
+  return (
+    <Form form={form} component={false}>
+      <Row style={Styles.salesRow}>
+        <Col span={12}>
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={onSearch}
+          />
+        </Col>
+      </Row>
+
+      <Table
+        components={{
+          body: {
+            cell: EditableCell
+          }
+        }}
+        bordered
+        dataSource={data}
+        columns={mergedColumns}
+        rowClassName="editable-row"
+        pagination={{
+          onChange: cancel
+        }}
+      />
+      <Row style={Styles.salesRow}>
+      <Row>
+        <Col style={Styles.salesRowC1}>
+          {" "}
+          <Button type="primary">Create Report</Button>
+        </Col>
+        <Col>
+          {" "}
+          <Button type="primary">Print PDF</Button>
+        </Col>
+        </Row>
+        <Col>
+          {" "}
+          <Button type="danger">Clear History</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+// ReactDOM.render(<EditableTable />, mountNode);
+export default EditableTable;
